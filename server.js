d7683a9fce7662db7ee3dd00cd7085fe4ee108d1
Ernesto Valenzuela <ev@evconsulting-inc.com>
2023-01-16 02:48:08 -0600

    Updated server.js; added Procfile

--- server.js
+++ server.js
@@ -12,11 +12,12 @@
 
 const express=require("express");
 const app=express();
+const PORT=process.env.PORT || 5000;
 app.use(express.static("public"));
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("Server started on port 3000");
 });
  
