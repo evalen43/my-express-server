164c7020f32666de49d9ddd9db9fb615a2411bbe
Ernesto Valenzuela <ev@evconsulting-inc.com>
2024-01-24 01:32:07 -0600

    update website

--- server.js
+++ server.js
@@ -10,14 +10,17 @@
 // }).listen(3000, function(){
 //     console.log("Server started on port 3000")});
 
-const express=require("express");
-const app=express();
-const PORT=process.env.PORT || 5000;
-app.use(express.static("public"));
-app.get("/",function(req,res){
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static("resources")); // Changed from "public" to "resources"
+
+app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app.listen(PORT, function(){
-    console.log("Server started on port 5000");
+
+app.listen(PORT, function() {
+    console.log(`Server started on port ${PORT}`);
 });
  
