080ac0e71a3502bfe50244a4a05a1c42c38a8656
Ernesto Valenzuela <ev@evconsulting-inc.com>
2024-01-30 15:30:59 -0600

    website update 1/30/24 3:30 pm

--- webhook.php
+++ webhook.php
@@ -12,8 +12,9 @@ $calculatedHash = 'sha1=' . hash_hmac('sha1', $payload, $secret, false);
 // Verify hash
 if (hash_equals($headerHash, $calculatedHash)) {
     // Execute command (e.g., git pull)
-    // exec('cd /home/evciztma/repositories/my-express-server && git pull');
-    exec('cd /home/evciztma/public_html && git pull');
+    exec('cd /home/evciztma/repositories/my-express-server && git pull');
+    // Synchronize files
+    exec('rsync -r --update --delete /home/evciztma/repositories/my-express-server/ /home/evciztma/public_html/');
     // Add more commands as needed
     echo 'Deployment triggered!';
 } else {
